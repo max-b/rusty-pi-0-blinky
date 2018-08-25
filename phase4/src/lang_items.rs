ea3ca91551363111cda04e949102a6befe66883a
Maxb <maxb.personal@gmail.com>
2018-08-24 22:18:27 -0400

    Running cargo fmt.

--- phase4/src/lang_items.rs
+++ phase4/src/lang_items.rs
@@ -1,15 +1,16 @@
-#[lang = "eh_personality"] pub extern fn eh_personality() {}
 use core::panic::PanicInfo;
 
+#[lang = "eh_personality"]
+pub extern "C" fn eh_personality() {}
 
 #[panic_implementation]
+#[no_mangle]
 pub extern "C" fn panic_fmt(_panic: &PanicInfo) -> ! {
     loop {}
 }
 
 #[no_mangle]
-pub unsafe extern fn memcpy(dest: *mut u8, src: *const u8,
-                            n: usize) -> *mut u8 {
+pub unsafe extern "C" fn memcpy(dest: *mut u8, src: *const u8, n: usize) -> *mut u8 {
     let mut i = 0;
     while i < n {
         *dest.offset(i as isize) = *src.offset(i as isize);
@@ -19,15 +20,16 @@ pub unsafe extern fn memcpy(dest: *mut u8, src: *const u8,
 }
 
 #[no_mangle]
-pub unsafe extern fn memmove(dest: *mut u8, src: *const u8,
-                             n: usize) -> *mut u8 {
-    if src < dest as *const u8 { // copy from end
+pub unsafe extern "C" fn memmove(dest: *mut u8, src: *const u8, n: usize) -> *mut u8 {
+    if src < dest as *const u8 {
+        // copy from end
         let mut i = n;
         while i != 0 {
             i -= 1;
             *dest.offset(i as isize) = *src.offset(i as isize);
         }
-    } else { // copy from beginning
+    } else {
+        // copy from beginning
         let mut i = 0;
         while i < n {
             *dest.offset(i as isize) = *src.offset(i as isize);
@@ -38,7 +40,7 @@ pub unsafe extern fn memmove(dest: *mut u8, src: *const u8,
 }
 
 #[no_mangle]
-pub unsafe extern fn memset(s: *mut u8, c: i32, n: usize) -> *mut u8 {
+pub unsafe extern "C" fn memset(s: *mut u8, c: i32, n: usize) -> *mut u8 {
     let mut i = 0;
     while i < n {
         *s.offset(i as isize) = c as u8;
@@ -48,13 +50,13 @@ pub unsafe extern fn memset(s: *mut u8, c: i32, n: usize) -> *mut u8 {
 }
 
 #[no_mangle]
-pub unsafe extern fn memcmp(s1: *const u8, s2: *const u8, n: usize) -> i32 {
+pub unsafe extern "C" fn memcmp(s1: *const u8, s2: *const u8, n: usize) -> i32 {
     let mut i = 0;
     while i < n {
         let a = *s1.offset(i as isize);
         let b = *s2.offset(i as isize);
         if a != b {
-            return a as i32 - b as i32
+            return a as i32 - b as i32;
         }
         i += 1;
     }
